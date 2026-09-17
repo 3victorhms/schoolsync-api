bad13f67f38a5118663a84bef5271fa01ee38bf0
victorhmsdev <victorhugomendesoares@gmail.com>
2026-09-17 16:57:15 -0300

    voltar

--- schoolsync-api/src/main/java/br/cefetmg/schoolsync_api/dto/usuario/UsuarioRequestDTO.java
+++ schoolsync-api/src/main/java/br/cefetmg/schoolsync_api/dto/usuario/UsuarioRequestDTO.java
@@ -23,10 +23,10 @@ public class UsuarioRequestDTO {
     @Size(max = 200, message = "O email deve ter no máximo 200 caracteres")
     private String email;
 
-    @Size(min = 6, message = "A senha deve ter no mínimo 6 caracteres")
-    @Pattern(regexp = "^$|.{6,}", message = "A senha deve ter no mínimo 6 caracteres")
+    // n coloquei obrigatória pq na hora de dar update n é obrigatório
+      @Size(min = 6, message = "A senha deve ter no mínimo 6 caracteres")
     private String senha;
-
+    
     @Size(max = 750000, message = "A foto de perfil é muito grande")
     private String foto;
 }
