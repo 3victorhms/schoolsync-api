b88b58bcba163b40c0bc2ca64cf8583ecc7b7c51
victorhmsdev <victorhugomendesoares@gmail.com>
2026-09-20 12:38:06 -0300

    adição da url do domínio netlify no cors

--- schoolsync-api/src/main/java/br/cefetmg/schoolsync_api/security/SecurityConfig.java
+++ schoolsync-api/src/main/java/br/cefetmg/schoolsync_api/security/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:8100", "https://localhost", "capacitor://localhost"));
+        configuration.setAllowedOrigins(List.of("http://localhost:8100", "https://localhost", "capacitor://localhost", "https://ppschoolsync.netlify.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setExposedHeaders(List.of("Authorization"));
